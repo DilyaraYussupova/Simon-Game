7d216e2dde407380a9187ffdb3ac3a567d4abe15
Your Name <you@example.com>
2018-09-24 12:55:42 -0700

    html/css done

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+/*----- app's state (variables) -----*/
+var computerSequence = [];
+var userSequence = [];
+var score = 0;
+var level = 0;
 
+/*----- cached element references -----*/
+var cardsBox = document.getElementById('img-grid');
+
+
+
+/*----- event listeners -----*/
+playButton.addEventListener('click', playButtonClick);
+
+/*----- functions -----*/
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("JS Loaded");
+});
+
+function playButtonClick(e) {
+   
+}
