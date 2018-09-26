731be8d6bce3103852b89bb5addb6cb0804f4261
Your Name <you@example.com>
2018-09-26 14:07:56 -0700

    final version: no sound and difficulty level set

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ userSequence = [];
 computerSequence = [];
 var id, img, level = 0, score = 0;
 
+const timeLookUp = {
+  easy: {
+     timeBetween: 2000,
+     timeToDisplay: 1800,
+  },
+  medium: {
+     timeBetween: 1500,
+     timeToDisplay: 1300,
+  },
+  hard: {
+     timeBetween: 1000,
+     timeToDisplay: 800,
+  }
+}
+
     $('#levelEasy').click(function() {
         startSequence();
     })
@@ -16,12 +31,13 @@ var id, img, level = 0, score = 0;
         $('.imgGrid').off('click')
         /* User img listener */
         $('.imgGrid').click(function() {
+            $message = $('.instruction').html("Play!");
             id = $(this).attr('id');
             img = $(this).attr('class');
             console.log(id + " " + img);
-            addOpacityUser(id, img);
             userSequence.push(id);
             //console.log(userSequence);
+            addOpacityUser(id, img);
 
         // check whether the order and img of user sequence are correct
             if(!userCorrect()) {
@@ -33,12 +49,11 @@ var id, img, level = 0, score = 0;
             if(userSequence.length === computerSequence.length) {
                 // increment score each time sequence is correc
                 ++score;
-                console.log(score);
+                //console.log(score);
                 $('.scoreBox').text("Score: " + score);
-
                 userSequence = [];
+                //$message = $('.instruction').html(" ");
                 startSequence();
-                $message = $('.instruction').html(" ");
             }
         })
     }
@@ -47,8 +62,9 @@ function addOpacityUser(id, img) {
     $('#' + id).addClass(img+"-tinted");
     setTimeout(function() {
         $('#' + id).removeClass(img+"-tinted");
-        $message = $('.instruction').html("Watch!");
-        console.log($message);
+        // if (userSequence.length === 3) {
+        //     $message = $('.instruction').html("Dilyara!!");
+        //     }
     }, 1800); 
 }
 /* compare user sequence against computer's */
@@ -62,16 +78,23 @@ function userCorrect() {
 }
 
 function gameOver() {
-    $message = $('.instruction').html("Game Over");
+    $message = $('.instruction').html("Game Over!").css({'color':'red','font-weight': 'bold'});
+    $('.levelBox').css({'color':'red','font-weight': 'bold'});
+    $('.scoreBox').css({'color':'red','font-weight': 'bold'});
   console.log("game over");
+  $('.resetBtn').click(function() {
+      resetGame();
+  })
 }
 /* Reset the game */
 function resetGame() {
     userSequence = [];
     computerSequence = [];
     level = 0;
+    $('.levelBox').text("Level: " + level).css({'color':'white','font-weight': 'normal'});;
     score = 0;
-    $message = $('.instruction').html("Watch!");
+    $('.scoreBox').text("Score: " + score).css({'color':'white','font-weight': 'normal'});;
+    $message = $('.instruction').html("Watch!").css({'color':'white','font-weight': 'normal'});;
 }
 
 /* Computer sequence */
@@ -80,12 +103,14 @@ function startSequence(cb) {
     //console.log("Level: " + level);
     $('.levelBox').text("Level: " + level);
    getRandomNumber();
+   console.log(computerSequence.length);
    
    var i = 0;
    var myinterval = setInterval(function() {
       id = computerSequence[i];
       //console.log(id);
       img = $('#' + id).attr('class');
+      $message = $('.instruction').html("Watch!");
       console.log(id + " " + img);
       addOpacityComputer(id, img);
       i++;
@@ -99,6 +124,7 @@ function startSequence(cb) {
 /* Generate random number */
 function getRandomNumber() {
     var random = Math.floor(Math.random()*8);
+    console.log(random);
     computerSequence.push(random);
     //console.log(computerSequence);
 }
@@ -108,7 +134,18 @@ function addOpacityComputer(id, img) {
     $('#' + id).addClass(img+"-tinted");
     setTimeout(function() {
         $('#' + id).removeClass(img+"-tinted");
-        $message = $('.instruction').html("Play");
+        if(computerSequence.length === 1 && level === 1) {
+            //console.log(computerSequence.length);
+        $message = $('.instruction').html("Play!");
+        } else {
+            $message = $('.instruction').html("Watch!");
+            $message = $('.instruction').html("Play!"); 
+        }
+        //$message = $('.instruction').html("Play"); 
     }, 1800); 
 }
+
+$('#levelMedium').click(function() {
+    startSequence();
+})
 
